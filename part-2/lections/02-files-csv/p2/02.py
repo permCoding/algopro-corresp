fb403079d69b31d09369d0ae21c308ec3e03c46c
Андрей Беляков <ttxiom@gmail.com>
2022-11-17 12:40:35 +0500

    up

--- part-2/lections/02-files-csv/p2/02.py
+++ part-2/lections/02-files-csv/p2/02.py
@@ -6,7 +6,22 @@ file_name = "./csv/02.csv"
 f = open(file_name, mode='r', encoding='utf8')
 reader = csv.DictReader(f, delimiter=',')
 
-for row in reader:
-    print(row["rat"])
+
+def get_lst1(reader):
+    abits = []
+    for abit in reader:
+        abits.append(abit)
+    return abits
+
+def get_lst2(reader):
+    abits = []
+    for abit in reader:
+        abits += [abit]
+    return abits
 
 
+def get_lst3(reader):
+    return [abit for abit in reader]
+
+
+print(get_lst3(reader))
---
+++ part-2/lections/02-files-csv/p2/test.py
@@ -0,0 +1,8 @@
+
+abits = [1,2]
+next = [3,4,5,6,7,8]
+
+abits.append(222)  # 666
+abits.extend(next) 
+abits += [999]
+print(abits)
