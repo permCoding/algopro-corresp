a2be3784afbc4e1e88a4eb78f8461679e4b5d7f8
Андрей Беляков <ttxiom@gmail.com>
2022-11-17 14:33:04 +0500

    05.py

---
+++ part-2/lections/02-files-csv/p2/05.py
@@ -0,0 +1,29 @@
+import csv
+
+# '\r' - return caret
+# '\n' - new line
+
+
+def get_lst(file_name):
+    with open(file_name, mode='r', encoding='utf8') as f:
+        return [abit for abit in csv.DictReader(f, delimiter=',')]
+
+
+def set_lst(file_name, data):
+    with open(file_name, mode='w', encoding='utf8') as f:
+        writer = csv.DictWriter(f, lineterminator='\r', fieldnames=['id','name','gender','age','group'])
+        # 
+        for row in data:
+            writer.writerow(row)
+
+
+file_name = "./csv/students.csv"
+studs = get_lst(file_name)  # получить список объектов
+filtred = filter(lambda x: x['gender']=='1', studs)  # отфильтровать по значению
+sorted = sorted(filtred, key=lambda x: x["name"])  # сортировать по полю
+
+set_lst("./csv/mens.csv", sorted)
+
+'''
+сохранить изменённые данные в новый файл
+'''
