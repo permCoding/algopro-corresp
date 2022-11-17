d424932cb6300e19fac03b908298bfdaab1b336c
Андрей Беляков <ttxiom@gmail.com>
2022-11-17 14:36:55 +0500

    05.py

--- part-2/lections/02-files-csv/p2/05.py
+++ part-2/lections/02-files-csv/p2/05.py
@@ -1,8 +1,5 @@
 import csv
 
-# '\r' - return caret
-# '\n' - new line
-
 
 def get_lst(file_name):
     with open(file_name, mode='r', encoding='utf8') as f:
@@ -11,8 +8,9 @@ def get_lst(file_name):
 
 def set_lst(file_name, data):
     with open(file_name, mode='w', encoding='utf8') as f:
-        writer = csv.DictWriter(f, lineterminator='\r', fieldnames=['id','name','gender','age','group'])
-        # 
+        writer = csv.DictWriter(f, delimiter=',', \
+            lineterminator='\r', fieldnames=['id','name','gender','age','group'])
+        writer.writeheader()
         for row in data:
             writer.writerow(row)
 
@@ -22,8 +20,10 @@ studs = get_lst(file_name)  # получить список объектов
 filtred = filter(lambda x: x['gender']=='1', studs)  # отфильтровать по значению
 sorted = sorted(filtred, key=lambda x: x["name"])  # сортировать по полю
 
-set_lst("./csv/mens.csv", sorted)
+set_lst("./csv/mens.csv", sorted)  # записать данные в файл
 
 '''
 сохранить изменённые данные в новый файл
+'\r' - return caret
+'\n' - new line
 '''
