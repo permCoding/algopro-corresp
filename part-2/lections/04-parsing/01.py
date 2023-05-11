5819d1f8c311f71aefce646896535841921b9073
Андрей Беляков <ttxiom@gmail.com>
2023-05-11 23:17:34 +0500

    up

--- part-2/lections/04-parsing/01.py
+++ part-2/lections/04-parsing/01.py
@@ -6,10 +6,3 @@ resp.encoding = "utf8"
 html = resp.text
 
 print(html)
-
-'''
-https://www.championat.com/football/_england/tournament/4013/table/  
-https://www.championat.com/hockey/_superleague/tournament/5077/table/  
-https://www.soccer.ru/main/table
-https://www.sports.ru/rfpl/table/
-'''
---
+++ part-2/lections/04-parsing/readme.txt
@@ -0,0 +1,8 @@
+### Сбор данных с сайтов  
+
+> https://www.championat.com/football/_england/tournament/4013/table/  
+> https://www.championat.com/hockey/_superleague/tournament/5077/table/  
+> https://www.soccer.ru/main/table  
+> https://www.sports.ru/rfpl/table/  
+
+---  
