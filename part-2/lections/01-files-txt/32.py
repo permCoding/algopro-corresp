c8467ff2373efa3e593eee173db998a5324070d5
Андрей Беляков <ttxiom@gmail.com>
2023-05-12 14:52:51 +0500

    up

---
+++ part-2/lections/01-files-txt/32.py
@@ -0,0 +1,18 @@
+def get_records(filename, sep=','):
+    abiturs = []
+    with open(filename, 'r', encoding='utf8') as f:
+        for line in f:
+            t = line.split(sep)
+            abiturs.append([int(t[0]), t[1], int(t[2])])
+    return abiturs
+
+
+
+min_rating = 232
+abiturs = get_records('./txt/abiturs.csv')
+
+filtred = [ab for ab in abiturs if ab[2]>=min_rating]
+ab_sorted = sorted(filtred, key=lambda x: x[2], reverse=True)
+
+for ab in ab_sorted:
+    print(*ab)
