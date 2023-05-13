c882970d470d09cd9169552961da345dba20f270
Андрей Беляков <ttxiom@gmail.com>
2023-05-13 13:55:02 +0500

    sort

--- part-2/lections/01-files-txt/99.py
+++ part-2/lections/01-files-txt/99.py
@@ -1,4 +1,6 @@
 def scompare(a, b):
+    if a == '': return False
+    if b == '': return True
     if a[0] == b[0]: 
         return scompare(a[1:], b[1:])
     if alph.find(a[0]) > alph.find(b[0]): 
@@ -12,15 +14,31 @@ alph = alph_lower + alph_upper
 print(alph.lower())
 
 lst = [
+    'еёе',
     'ееЕ',
-    'еееЕ'
+    'еееЕ',
+    'ё',
+    'е',
+    'Ё',
+    'Е',
+    'деревня',
+    'жаба',
     'Ёбиюпдмчгй', 
     'ЕеТизп', 
     'Ёшюдекфймн'
 ]
-
 print( lst[0] > lst[1] )
 print( scompare(lst[0],lst[1]) )
 
 print( lst[1] > lst[2] )
 print( scompare(lst[1],lst[2]) )
+
+# lst = [9,8,0,0,3,4,1,2]
+
+n = len(lst)
+for i in range(n-1):
+    for j in range(n-1-i):
+        # if lst[j]>lst[j+1]:
+        if scompare(lst[j],lst[j+1]):
+            lst[j], lst[j+1] = lst[j+1], lst[j]
+for elm in lst: print(elm)
